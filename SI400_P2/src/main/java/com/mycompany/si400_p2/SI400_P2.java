9369c36872ac32087c53e82bc5f6fc60ae48d5e0
gguilherme-gritti <78884167+gguilherme-gritti@users.noreply.github.com>
2022-11-03 17:01:32 -0300

    create project

---
+++ SI400_P2/src/main/java/com/mycompany/si400_p2/SI400_P2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.si400_p2;
+
+/**
+ *
+ * @author Gritti
+ */
+public class SI400_P2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
